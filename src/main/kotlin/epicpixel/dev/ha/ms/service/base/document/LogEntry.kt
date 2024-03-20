08859f65b692318e809430a8158c614e96ab9b73
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2024-03-20 12:54:50 -0300

    Merge de cadastro-usuario-crud para crud-de-logs.

--- src/main/kotlin/epicpixel/dev/ha/ms/service/base/document/LogEntry.kt
+++ src/main/kotlin/epicpixel/dev/ha/ms/service/base/document/LogEntry.kt
@@ -1,4 +1,5 @@
-package epicpixel.dev.ha.ms.service.base.entity
+package epicpixel.dev.ha.ms.service.base.document
+
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.DBRef
@@ -7,16 +8,12 @@ import org.springframework.data.mongodb.core.mapping.Field
 import java.time.Instant
 
 @Document("logs")
-data class LogEntry (
-
+data class LogEntry(
     @Id val id: ObjectId = ObjectId(),
-
-    val user: Long? = null, // TODO: trocar por: @DBRef val user: UserDocument? = null,
+    @DBRef val user: UserDocument? = null,
     val createdAt: Instant = Instant.now(),
-
     @Field("type") val level: LogLevel = LogLevel.INFO,
-    val details : String,
-
+    val details: String,
 )
 
 enum class LogLevel {
